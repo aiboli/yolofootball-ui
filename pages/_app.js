9920a94b35803bf3a9657fb9ac85c98039e89ee1
Aibo Li <albertlabtech@gmail.com>
2026-04-19 15:41:16 -0700

    update font size

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import "./index.css";
+import { Roboto } from "next/font/google";
 import AppContextProvider from "../helper/AppContextProvider";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <AppContextProvider>
-      <Component {...pageProps} />
-    </AppContextProvider>
+    <div className={roboto.className}>
+      <AppContextProvider>
+        <Component {...pageProps} />
+      </AppContextProvider>
+    </div>
   );
 }
